b79d9153b3474e98f0efb28968d6096fe74a9916
denys.litvinov <vuggshoes@gmail.com>
2024-09-27 17:08:11 +0300

    first-commit

---
+++ eureka-service/src/test/java/com/dlitv/cloud/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlitv.cloud.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
