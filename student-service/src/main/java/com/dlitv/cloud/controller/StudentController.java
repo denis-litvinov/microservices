fddf693b8f6d466031f0b478e2ede176967fb5dd
denys.litvinov <vuggshoes@gmail.com>
2024-09-27 17:45:11 +0300

    refactor

--- student-service/src/main/java/com/dlitv/cloud/controller/StudentController.java
+++ student-service/src/main/java/com/dlitv/cloud/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.dlitv.cloud.request.CreateStudentRequest;
 import com.dlitv.cloud.response.StudentResponse;
 import com.dlitv.cloud.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
--- student-service/src/main/java/com/dlitv/cloud/entity/StudentEntity.java
+++ student-service/src/main/java/com/dlitv/cloud/entity/StudentEntity.java
@@ -1,5 +1,6 @@
 package com.dlitv.cloud.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
